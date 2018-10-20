67fb9f44b5d1fa59b62113045f3486eb7fd2946e
teerapong300 <teerapong.intoom@github.com>
2018-10-20 20:42:55 +0700

    first commit

---
+++ pull_data_yahoo.R
@@ -0,0 +1,18 @@
+# Pull data from yahoo
+source("download_library.R")
+stock_names <- c("AAPL","MSFT","AMZN","FB","JPM","XOM","GOOG","GOOGL","JNJ")
+start <- "2008-1-1" 
+end <- "2018-1-1" 
+
+stock_series <- data.frame()
+
+for ( name in stock_names) {
+  get_data <- getSymbols(name, from = start, to = end, auto.assign = F)[ ,4]
+  names(get_data) <- name
+  stock_series <- merge.xts(get_data, stock_series)
+}
+
+write.csv(as.data.frame(stock_series),"stock_series.csv")
+
+
+
---
+++ stylized_facts.R
@@ -0,0 +1,20 @@
+
+# Calculate log returns
+log_ret <- Return.calculate(stock_series, method = "log")
+
+# Nomality test using Shapiro Test
+lshap <- lapply(as.data.frame(log_ret), shapiro.test)
+lres <- sapply(lshap, `[`, c("statistic","p.value"))
+des <- t(lres)
+
+ladf <- lapply(as.data.frame(log_ret), function(x) {Box.test(x,lag = 20, type = "Ljung-Box")})
+des <- sapply(ladf, `[`, c("statistic", "p.value"))
+des <- t(des)
+
+decompose(log_ret[,1])
+udf_data <- as.data.frame(log_ret)
+udf_data[is.na(udf_data)] <- 0
+udf_test <- lapply(udf_data, function(x) {ur.df(x, type = "trend", lags = 10, selectlags = "BIC")})
+
+
+
